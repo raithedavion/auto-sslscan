44cd6af3c9240f3901d8c2b170362fe420fd6e6e
raithedavion <raithedavion@gmail.com>
2025-03-07 10:56:56 -0600

    Updated to Python3

--- auto-sslscan.py
+++ auto-sslscan.py
@@ -17,64 +17,61 @@ instructions =  "auto-sslscan - v0.4 ( https://github.com/attackdebris/auto-ssls
                 "\nUSAGE: auto-sslscan.py [nmap-ouput.xml] [output-file]" 
 
 if len(sys.argv) <3 or sys.argv[1] == "-h" or sys.argv[1] == "--h" or sys.argv[1] == "-help" or sys.argv[1] == "--help":
-        print instructions
+        print(instructions)
         sys.exit()
 elif len(sys.argv) >3:
-	print instructions
-	sys.exit()
+        print (instructions)
+        sys.exit()
 elif len(sys.argv) ==3:
-	nmapxml = sys.argv[1]
-	myfile = sys.argv[2]
-	f = open(myfile, 'w')
-	print >> f, "===================================================================="
-	print >> f, "auto-sslscan - v0.4 ( https://github.com/attackdebris/auto-sslscan )"
-	print >> f, "====================================================================\n"
-	f.close
-	temp = ".tmp-auto-sslscan"
-	sslservices = myfile.replace('.txt', '')+"-ssl-services.txt"
-	f = open(temp, 'w')
-	f.close
-	f = open(sslservices, 'w')
-	f.close
-	print "auto-sslscan - v0.4 ( https://github.com/attackdebris/auto-sslscan )\n"
+        nmapxml = sys.argv[1]
+        myfile = sys.argv[2]
+        with open(myfile, 'w') as f:
+                f.write("====================================================================\n")
+                f.write("auto-sslscan - v0.4 ( https://github.com/attackdebris/auto-sslscan )\n")
+                f.write("====================================================================\n\n")
+        temp = ".tmp-auto-sslscan"
+        sslservices = myfile.replace('.txt', '') + "-ssl-services.txt"
+        with open(temp, 'w'):
+                pass
+        with open(sslservices, 'w'):
+                pass 
+        print("auto-sslscan - v0.4 ( https://github.com/attackdebris/auto-sslscan )\n")
 
 def report_parser(report):
-    ''' Parse the Nmap XML report '''
-    for host in report.hosts:
-        ip = host.address
+        ''' Parse the Nmap XML report '''
+        for host in report.hosts:
+                ip = host.address
 
-            # Get the port and service
-            # objects in host.services are NmapService objects
-	for s in host.services:
-            # Check if port is open
-		if s.open():
-			serv = s.service
-                	port = s.port
-	        	tunnel = s.tunnel
+                # Get the port and service
+                # objects in host.services are NmapService objects
+                for s in host.services:
+                # Check if port is open
+                        if s.open():
+                                serv = s.service
+                                port = s.port
+                                tunnel = s.tunnel
 
-			# Perform some action on the data
-                	print_data(ip, port, tunnel)
+                        # Perform some action on the data
+                                print_data(ip, port, tunnel)
+
 
 def print_data(ip, port, tunnel):
-	''' Do something with the nmap data '''
-	if tunnel != '':
-		f = open(temp, 'w')
-		print >> f, '{0}:{1}'.format(ip,port) 
-		print 'Performing sslscan of {0}:{1}'.format(ip,port)
-		f.close
+        ''' Do something with the nmap data '''
+        if tunnel != '':
+                with open(temp, 'w') as f:
+                        f.write('{0}:{1}\n'.format(ip, port))
+                print('Performing sslscan of {0}:{1}'.format(ip, port))
+                with open(sslservices, 'a+') as f:
+                        f.write('{0}:{1}\n'.format(ip, port))
+                with open(myfile, 'a+') as f:
+                        subprocess.call(["sslscan", "--no-failed", "--targets=" + temp], stdout=f)
 
-		f = open(sslservices, 'a+')
-                print >> f, '{0}:{1}'.format(ip,port)
-		f.close
-
-		f = open(myfile, 'a+')	
-		subprocess.call(["sslscan", "--no-failed", "--targets="+temp], stdout=f)
-		f.close
 def end():
-		print "\nsslscan results saved to: {}".format(myfile)
-		print "SSL services list saved to: {}".format(sslservices)
-		os.remove(temp)
-			
+        print("\nsslscan results saved to: {}".format(myfile))
+        print("SSL services list saved to: {}".format(sslservices))
+        os.remove(temp)
+
+
 def main():
     report = NmapParser.parse_fromfile(nmapxml)
     report_parser(report)
